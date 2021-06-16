a0e5301cb7ef291de8a355e59844db25195964b4
dapx <dap1995@gmail.com>
2021-06-16 00:46:57 -0300

    Send a hello world notification

--- src/nativeMain/kotlin/hello.kt
+++ src/nativeMain/kotlin/hello.kt
@@ -1,4 +1,15 @@
+import libnotify.notify_init
+import libnotify.notify_notification_show
+import libnotify.notify_uninit
+
 fun main() {
-  println("Hello Kotlin/Native!")
+  notify_init("Kotlin/Native")
+  val notification = libnotify.notify_notification_new(
+    summary = "Teste",
+    body = "Hello Kotlin/Native!",
+    icon = "dialog-information"
+  )
+  notify_notification_show(notification, error = null)
+  notify_uninit()
 }
 
